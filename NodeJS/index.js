b16644eb90b9a9e20881ffd13714fba07f9be649
Bilal Afzaal <bafzaal@uwo.ca>
2019-11-22 21:37:00 -0500

    Implementing access control logic

---
+++ NodeJS/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+var songController = require('./controllers/songController.js');
+var reviewController = require('./controllers/reviewController.js');
+var userController = require('./controllers/userController.js');
+
+mongoose.connect('mongodb+srv://BilalAfzaal:mongodb@cluster0-firjs.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true }, (err) => {
+    if (!err)
+        console.log('MongoDB Connected!')
+    else
+        console.log('MongoDB Connection Failed: ' + JSON.stringify(err, undefined, 2));
+});
+module.exports = mongoose;
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    next();
+  });
+
+var port = process.env.PORT || 8080; 
+app.listen(port, () => console.log('Server is running at port 8080'));
+
+// app.use('/', )
---
+++ NodeJS/models/song.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Song = mongoose.model('Song', 
+{
+    objectID: {type: Number, require:true, unique:true},
+    songTitle: {type: String, require:true},
+    artist: {type: String, require:true},
+    album: {type: String},
+    year: {type: String},
+    comment: {type: String},
+    genre: {type: String},
+    avgRating: {type: Number, require:true, default:0},
+    numReviews: {type: Number, require:true, default:0},
+    numRating: {type: Number, require:true, default:0},
+});
+
+module.exports = { Song };
