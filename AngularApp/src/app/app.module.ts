80caa16d366b3a8d71af6612df46772ca7e874fa
Bilal Afzaal <bafzaal@uwo.ca>
2019-11-27 17:36:33 -0500

    Sorted songs by average rating

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { DataTableModule } from "@pascalhonegger/ng-datatable";
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
